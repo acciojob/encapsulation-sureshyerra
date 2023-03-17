19ebbc55f6c1f8980f7150808a3514a448e00415
unknown <sureshyerr897@gmail.com>
2023-03-17 15:44:55 +0530

    assignment submitted

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+     private String name;
+
+     public String setter(String setname){
+          this.name = setname;
+          return name;
+     }
+     public void getter(){
+          System.out.println(name);
+     }
 }
