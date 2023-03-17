91128da7e3e6aafdb11813cae7e48bcb5e4afa12
unknown <sureshyerr897@gmail.com>
2023-03-17 16:12:33 +0530

    work done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,14 +1,13 @@
 package com.driver;
 
 public class Main {
-
     public static void main(String[] args) {
-
         RWOnly obj = new RWOnly();
 
-        obj.setter("encapsulation");
-        String ans = obj.getter();
+        obj.setName("encapsulation");
+
+        String ans = obj.getName();
+
         System.out.println(ans);
     }
-  
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,13 +1,16 @@
 package com.driver;
 
 public class RWOnly {
-     private String name;
+    private String name;
+    public String getName() {
+        return name;
+    }
 
-     public String getName() {
-          return name;
-     }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-     public void setName(String name) {
-          this.name = name;
-     }
+
+
+
 }
